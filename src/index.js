1ac88d73fb447a43b13343f1bb1a74677dbb6c31
Suha AB <abubakr.suha@gmail.com>
2023-06-18 22:17:23 +0300

    fixed login bug #29

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Importing Modules
 const fs = require('node:fs')
+const path = require('node:path');
 const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
 const { token } = require('../config.json');
 
@@ -8,15 +9,15 @@ const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
 // Client Commands Collection will allow easy use of the bot's commands
 client.commands = new Collection()
-const commandsPath = path.join(__dirname, 'commands')
+const commandsPath = path.join(__dirname, '../commands')
 const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
 
 for (const file of commandsFiles) {
     const filePath = path.join(commandsPath, file)
     const command = require(filePath)
-
     if ('data' in command && 'execute' in command){
-        client.command.set(command.data.name, command)
+        console.log(command.data.name)
+        client.commands.set(command.data.name, command)
     } else {
         console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 
