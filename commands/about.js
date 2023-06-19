79c1cbdd291691bf55029ffe93f91c762d4f6fda
Suha AB <abubakr.suha@gmail.com>
2023-06-19 23:31:03 +0300

    created About Embed (fixes #4, fixes #12, and fixes #13)

--- commands/about.js
+++ commands/about.js
@@ -1,15 +1,19 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('about')
     .setDescription('learn more about Pomo!'),
     async execute(interaction){
-        await interaction.reply(`${interaction.user.username} used the about command.`)
+        await interaction.reply({embeds: [aboutEmbed]})
     }
 
-
-
-
-
 }
+
+const aboutEmbed = new EmbedBuilder()
+.setTitle('About Pomo')
+.setDescription("Pomo is a time management bot based on The Pomodoro Technique (developed by [Francesco Cirillo](https:\/\/francescocirillo.com\/) in the 1980s). The Pomodoro Technique is a time managment method that uses a timer to break work into intervals of, typically, 25 minutes. These intervals are separated by short breaks, typically 5 minutes.")
+.setColor('ec3946')
+.addFields(
+    {name: "ㅤ", value: "Pomo was coded and developed by [Alcor](https://github.com/suha-ab) using discordJS."}
+)
