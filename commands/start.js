707d51497291cc71a55696acb72a3526daf1859b
Suha AB <abubakr.suha@gmail.com>
2023-07-01 22:20:12 +0300

    working on start embed #26

--- commands/start.js
+++ commands/start.js
@@ -7,13 +7,23 @@ const myPomos = [];
 
 function startCommad(interaction){
         const currTime = Date.now()
-        console.log(myPomos);
+        //console.log(myPomos);
         Pomo.printHello()
         myPomos.push(new Pomo(interaction))
         myPomos[myPomos.length - 1].startTimer();
-        console.log(myPomos)
+        //console.log(myPomos)
 
 }
+function createstartEmbed(interaction){
+    //console.log(interaction.member)
+    const startEmbed = new EmbedBuilder()
+    .setTitle(`${interaction.member.nickname}'s Pomo has been initialized!`)
+    .setDescription("Your Pomo has just started. Time to get the show on the road!")
+    .setColor('ec3946')
+    return startEmbed;
+}
+
+
 
 
 module.exports = {
@@ -22,7 +32,7 @@ module.exports = {
     .setDescription('Starts 1 Pomodoro timer.'),
     async execute(interaction){
         startCommad(interaction)
-        await interaction.reply("Timer has begun.");
+        await interaction.reply({embeds: [createstartEmbed(interaction)]});
 
     }
 
