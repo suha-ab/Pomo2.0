e2ca12a06cdedc677d15c05aec74a9b05b219dcc
Suha AB <abubakr.suha@gmail.com>
2023-07-08 22:20:40 +0300

    working on pause command #15

--- src/pomo.js
+++ src/pomo.js
@@ -1,9 +1,12 @@
 const { EmbedBuilder } = require('discord.js')
     module.exports  =     
-    // Pomo = 25 - 5 - 25 - 5 - 25 - 5 - 25 - 15
+    
     class Pomo {
-        constructor(interaction) {
+        constructor(interaction, currTimeoutInterval, remainingTime, numOfPomos) {
             this.interaction = interaction;
+            this.currTimeoutInterval = currTimeoutInterval;
+            this.remainingTime = remainingTime;
+            this.numOfPomos = numOfPomos;
         /*  
             const username = user;
             const startTime = start;
@@ -16,20 +19,23 @@ const { EmbedBuilder } = require('discord.js')
             this.numOfPomos += 1;
         }
     
+        // Pomo = 25 - 5 - 25 - 5 - 25 - 5 - 25 - 15
         startTimer(myPomos){
-            const finishEmbed = new EmbedBuilder()
-            .setTitle(`${this.interaction.member.nickname}'s Pomo has finished!`)
-            .setDescription(`Your Pomo has just finished, time for a break!`)
-            .setColor('ec3946')
+            const finishEmbed = this.createFinishEmbed()
 
             // run timer for 25 mins
-            setTimeout(()=>{
-                this.incrementPomo();
+            this.currTimeoutInterval = setTimeout(()=>{
+                this.incrementPomo()
                 this.interaction.followUp({embeds: [finishEmbed]})
-                myPomos.shift()
+                myPomos.shift() // needs corrections <- If another pomo starts before this one finishes, it will be shifted
                 //console.log(`myPomos after shift`)
                 //console.log(myPomos)
             },(10000))
+        }
+
+        pauseTimer(myPomos){
+            
+
         }
 
         getInteraction(){
@@ -38,6 +44,13 @@ const { EmbedBuilder } = require('discord.js')
     
         static printHello(){
             console.log("hello")
+        }
+
+        createFinishEmbed(){
+            return new EmbedBuilder()
+            .setTitle(`${this.interaction.member.nickname}'s Pomo has finished!`)
+            .setDescription(`Your Pomo has just finished, time for a break!`)
+            .setColor('ec3946')
         }
     
 
