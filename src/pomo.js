de23f01821bc72c72229fa464307f2517d1fec6f
Suha AB <abubakr.suha@gmail.com>
2023-07-13 20:16:20 +0300

    completed restart command (fixes #9, fixes #20, and fixes #21) + starting working on timer command #11

--- src/pomo.js
+++ src/pomo.js
@@ -109,6 +109,14 @@ const myPomos = require('./pomoArray');
             this.startTimer(myPomos)
         }
 
+        remainingInTimer(){
+            var now = Date.now()
+            var millis = now - this.currStartTime
+            var minutes = Math.floor(millis / 60000)
+            var seconds = ((millis % 60000) / 1000).toFixed(0)
+            return (seconds == 60 ? ((minutes + 1) + ":00") : minutes + ":" + (seconds < 10 ? "0" : "") + seconds)
+        }
+
         getInteraction(){
             return this.interaction;
         }
